2b8acec49848a1cb20d83b5108735740f94b1582
TOPBARD <ritikjuneja9897@gmail.com>
2024-03-02 12:33:19 +0530

    Admin guard and prettier workflow added

---
+++ backend/src/auth/guard/admin.guard.ts
@@ -0,0 +1,27 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { User } from 'src/user/schema/user.schema';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    // Accessing the user from the request object
+    const request = context.switchToHttp().getRequest();
+    const user = request.user as User;
+
+    // Check if the user is an admin
+    if (user && user.isAdmin) {
+      return true;
+    }
+
+    // If not an admin, deny access
+    throw new UnauthorizedException('User is not admin');
+  }
+}
