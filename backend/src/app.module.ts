cd320fa681e26b2e5e7f7597d4a424a171cda528
Topbard <ritikjuneja9897@gmail.com>
2025-01-26 14:08:42 +0530

    Fix:Redis-crashing Temp removed redis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,32 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { BookModule } from './books/book.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtConfigModule } from './shared/jwt/jwt.module';
 import { OrderModule } from './orders/orders.module';
-import * as redisStore from 'cache-manager-redis-store';
-import config from './config';
-import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, cache: true, load: [config] }),
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI as string),
-    CacheModule.registerAsync({
-      inject: [ConfigService],
-      isGlobal: true,
-      useFactory: async (configService: ConfigService) => ({
-        store: redisStore,
-        host: configService.get('REDIS_HOST'),
-        port: configService.get('REDIS_PORT'),
-        password: configService.get('REDIS_PASSWORD'),
-        no_ready_check: true,
-        ttl: 300,
-      }),
-    }),
     JwtConfigModule,
     OrderModule,
     BookModule,
